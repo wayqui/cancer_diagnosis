c8b8d2237ce1c477114ad661e94506f57b55d931
jose bustamante <joselobm@joses-MacBook-Pro.local>
2021-10-19 18:45:16 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Cancer diagnosis model using machine learning and KDD processes',
+    author='José Bustamante Morales',
+    license='',
+)
